de81c61f2dc94ea8ceebb94d4d6c19d08fec8926
drugpamir <toll_aa@mail.ru>
2025-01-28 01:16:48 +0300

    react-redux provider and setup store

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import React from "react";
+import { Provider } from "react-redux";
+import { setupStore } from "./redux/store";
 
 const rootElement = document.getElementById("root") as HTMLElement;
 const root = ReactDOM.createRoot(rootElement);
 
-root.render(<App />);
+root.render(
+  <Provider store={setupStore()}>
+    <App />
+  </Provider>,
+);

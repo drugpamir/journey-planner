b615f52d13dcaf7ad21deb7c58f7112facc0d2e1
drugpamir <toll_aa@mail.ru>
2025-01-28 20:41:50 +0300

    added NavItem component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { NavLink } from "react-router";
 import { TO_ABOUT } from "../pages/About";
 import { TO_LOCATIONS } from "../pages/Locations";
 import { TO_JOURNEYS } from "../pages/Journeys";
 import { TO_JOURNEY_EDITOR } from "../pages/JourneyEditor";
+import NavItem from "./NavItem";
 
-export const NavBar = () => {
+export const NavBar = ({ activeTo: activeLink }: { activeTo: string }) => {
+  console.log("activeTo:", activeLink);
   return (
     <nav className="navbar">
-      <NavLink to={TO_ABOUT}>О планировщике</NavLink>
-      <NavLink to={TO_LOCATIONS}>Локации</NavLink>
-      <NavLink to={TO_JOURNEYS}>Маршруты</NavLink>
-      <NavLink to={TO_JOURNEY_EDITOR}>Редактор</NavLink>
+      <NavItem to={TO_ABOUT}>О планировщике</NavItem>
+      <NavItem to={TO_LOCATIONS}>Локации</NavItem>
+      <NavItem to={TO_JOURNEYS}>Маршруты</NavItem>
+      <NavItem to={TO_JOURNEY_EDITOR}>Редактор</NavItem>
     </nav>
   );
 };
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,5 +3,5 @@ import React from "react";
 export const TO_ABOUT = "/about";
 
 export const About = () => {
-  return <h1>О планировщике прогулок</h1>;
+  return <h1>О планировщике</h1>;
 };

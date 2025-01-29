f566b65fe598fad2ab421e5aabb6ed63c0aa493a
drugpamir <toll_aa@mail.ru>
2025-01-29 12:29:06 +0300

    added County field to City

--- src/api/cityInfoApi.ts
+++ src/api/cityInfoApi.ts
@@ -1,22 +1,32 @@
 import { City } from "../models/City";
 import { fetchJson } from "./fetchUrl";
 
-const endpoint = process?.env?.CITIES_ENDPOINT;
-const apiKey = process?.env?.CITIES_API_KEY;
+console.log("process:", process);
+
+const endpoint = process.env?.CITIES_ENDPOINT;
+const apiKey = process.env?.CITIES_API_KEY;
 
 export async function fetchCityInfo(
   cityName: string,
   limit: number = 5,
 ): Promise<City> {
   const _url = `${endpoint}?q=${cityName}&limit=${limit}&appid=${apiKey}`;
+  console.log(_url);
   let city: City;
   try {
     const json = await fetchJson(_url);
-    const { name, local_names, state, lat, lon } = json[0];
-    city = new City(name, local_names, state, lat, lon);
+    city = new City(json[0]);
+    console.log("Loaded json:", json);
     console.log("Loaded city:", city);
   } catch {
-    city = new City("Moscow", { ru: "Москва" }, "Moscow", 33, 75);
+    city = new City({
+      name: "Moscow",
+      local_names: { ru: "Москва" },
+      state: "Moscow",
+      country: "RU",
+      lat: 33,
+      lon: 55,
+    });
     console.log("City NOT found");
   }
   return city;

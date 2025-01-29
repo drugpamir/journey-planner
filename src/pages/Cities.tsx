1952bc604bb60a713b6a8f82d4dabea4c00767c9
drugpamir <toll_aa@mail.ru>
2025-01-29 17:57:46 +0300

    adding city by UI

--- src/pages/Cities.tsx
+++ src/pages/Cities.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import AddCityForm from "../components/Form.AddCity";
+import CityList from "../components/CityList";
+import { City } from "../models/City";
+import { useAppSelector } from "../hooks/redux";
+import { citiesSlice } from "../redux/slice";
 
 export const TO_CITIES = "/cities";
 
 export const Cities = () => {
+  const removeCity = (city: City) => {};
+  const citiesState = useAppSelector((state) => state.cities);
+  console.log("citiesState:" + citiesState);
+
   return (
     <>
       <h1>Cities</h1>
       <AddCityForm></AddCityForm>
+      <CityList
+        cities={citiesState.cities}
+        title="Added cities"
+        removeCity={removeCity}
+      ></CityList>
     </>
   );
 };

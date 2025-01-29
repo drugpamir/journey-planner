0c8b683567af9040c96390dc5ceb45f41a477024
drugpamir <toll_aa@mail.ru>
2025-01-29 17:55:53 +0300

    added extraReducers (fetchCityInfo, getStorageCities, addCityToStorage)

--- src/redux/actionCreators.ts
+++ src/redux/actionCreators.ts
@@ -1,12 +1,28 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { City } from "../models/City";
-import { fetchCityInfo } from "../api/cityInfoApi";
+import { cityStorageApi } from "../api/storageApi";
+import { cityInfoOpenApi } from "../api/cityInfoApi";
 
-export const getAllCityInfo = createAsyncThunk(
+export const fetchCityInfo = createAsyncThunk(
   "cities/info",
-  async (cityName: string) => {
-    const city: City = await fetchCityInfo(cityName);
-    console.log(city);
+  async (cityName: string /*, thunkAPI*/) => {
+    const city: City = await cityInfoOpenApi.fetchInfo(cityName);
     return city;
+  },
+);
+
+export const getStorageCities = createAsyncThunk(
+  "cities/added",
+  async (/*data, thunkAPI*/) => {
+    const response = await cityStorageApi.getCities();
+    return response;
+  },
+);
+
+export const addCityToStorage = createAsyncThunk(
+  "cities/create",
+  async (cityParams: Omit<City, "id"> /*, thunkAPI*/) => {
+    const response = await cityStorageApi.addCity(cityParams);
+    return response;
   },
 );

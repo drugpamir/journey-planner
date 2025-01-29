6c95f9992e8bf28f2beecc71345f2c413734644c
drugpamir <toll_aa@mail.ru>
2025-01-29 17:51:17 +0300

    fetchCityInfo encapsulated to CityInfoAPI

---
+++ src/api/cityInfoApi.open.ts
@@ -0,0 +1,24 @@
+import { City, CityByDefault, parseToCity } from "../models/City";
+import { CityInfoAPI } from "./cityInfoApi";
+import { fetchJson } from "./fetchUrl";
+
+const endpoint = process.env?.CITIES_ENDPOINT;
+const apiKey = process.env?.CITIES_API_KEY;
+
+export class CityInfoOpenApi implements CityInfoAPI {
+  async fetchInfo(cityName: string, limit: number = 5): Promise<City> {
+    const _url = `${endpoint}?q=${cityName}&limit=${limit}&appid=${apiKey}`;
+    console.log(_url);
+    let city: City;
+    try {
+      const json = await fetchJson(_url);
+      city = parseToCity(json[0]);
+      console.log("Loaded json:", json);
+      console.log("Loaded city:", city);
+    } catch {
+      city = CityByDefault;
+      console.log("City NOT found");
+    }
+    return city;
+  }
+}
--- src/api/cityInfoApi.ts
+++ src/api/cityInfoApi.ts
@@ -1,24 +1,8 @@
-import { City, CityByDefault, parseToCity } from "../models/City";
-import { fetchJson } from "./fetchUrl";
+import { City } from "../models/City";
+import { CityInfoOpenApi } from "./cityInfoApi.open";
 
-const endpoint = process.env?.CITIES_ENDPOINT;
-const apiKey = process.env?.CITIES_API_KEY;
-
-export async function fetchCityInfo(
-  cityName: string,
-  limit: number = 5,
-): Promise<City> {
-  const _url = `${endpoint}?q=${cityName}&limit=${limit}&appid=${apiKey}`;
-  console.log(_url);
-  let city: City;
-  try {
-    const json = await fetchJson(_url);
-    city = parseToCity(json[0]);
-    console.log("Loaded json:", json);
-    console.log("Loaded city:", city);
-  } catch {
-    city = CityByDefault;
-    console.log("City NOT found");
-  }
-  return city;
+export interface CityInfoAPI {
+  fetchInfo(cityName: string, limit?: number): Promise<City>;
 }
+
+export const cityInfoOpenApi: CityInfoAPI = new CityInfoOpenApi();

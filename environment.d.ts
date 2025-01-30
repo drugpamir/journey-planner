413fcabaafd680b4c5c0c53d8dd4c9e61b57a126
drugpamir <toll_aa@mail.ru>
2025-01-30 14:06:49 +0300

    added firebaseConfig with envs

--- environment.d.ts
+++ environment.d.ts
@@ -3,6 +3,10 @@ declare global {
     interface ProcessEnv {
       CITIES_API_KEY: string;
       CITIES_ENDPOINT: string;
+      FIREBASE_API_KEY: string;
+      FIREBASE_PROJECT: string;
+      FIREBASE_SENDER: string;
+      FIREBASE_APP_ID: string;
     }
   }
 }
